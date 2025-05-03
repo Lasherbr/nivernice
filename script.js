a71d372c6cc322233dfcc0f9a5cf3f1b0e13c15e
Lasherbr <ricardi@gmail.com>
2025-05-02 22:16:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const slides = document.querySelectorAll('.slide');
 let current = 0;
+
+function showNextSlide() {
+    slides[current].classList.remove('active');
+    current = (current + 1) % slides.length;
+    slides[current].classList.add('active');
+}
+
 if (slides.length > 0) {
-    slides[current].style.display = 'block';
-    setInterval(() => {
-        slides[current].style.display = 'none';
-        current = (current + 1) % slides.length;
-        slides[current].style.display = 'block';
-    }, 4000);
+    slides[current].classList.add('active');
+    setInterval(showNextSlide, 4000);
 }
